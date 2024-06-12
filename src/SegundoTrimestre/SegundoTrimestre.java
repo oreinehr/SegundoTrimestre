a8547a025509e3fefa8e89bb8ee9cb1a4083e014
reinehrgui <guilherme-reinehr@educar.rs.gov.br>
2024-06-12 08:23:02 -0300

    bomdia

---
+++ src/SegundoTrimestre/SegundoTrimestre.java
@@ -0,0 +1,11 @@
+package SegundoTrimestre;
+
+import java.util.Scanner;
+public class SegundoTrimestre {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+
+
+    }
+
+}
